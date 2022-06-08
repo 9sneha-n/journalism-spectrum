18ee76ee9230e3624f28707e8ccddfaa4e9d88cd
Sneha N <9sneha.n@gmail.com>
2022-06-08 21:23:43 +0530

    mobile responsive

--- src/Components/HeaderGrid.jsx
+++ src/Components/HeaderGrid.jsx
@@ -4,7 +4,7 @@ import './HeaderGrid.css';
 export default function HeaderGrid({ ...props }) {
     return (
         <div className={'HeaderGrid ' + props.rowHeader}>
-            <h4>{props.title}</h4>
+            <h4 className='GridHeading'>{props.title}</h4>
         </div>
     )
 }
